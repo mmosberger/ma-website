57843175d0d6b8634612a17f1e231d943c6dbc44
Knirpsii <michel.j.mosberger@gmail.com>
2021-08-19 18:16:28 +0200

    -

--- pages/test/[testId]/sleep.js
+++ pages/test/[testId]/sleep.js
@@ -1,5 +1,5 @@
 import {useRouter} from "next/router";
-import {useState, useEffect} from 'react';
+import {useState} from 'react';
 
 
 const Sleep = ({data}) => {
@@ -7,26 +7,26 @@ const Sleep = ({data}) => {
     const router = useRouter()
     const {testId} = router.query
 
+
     const [sleep_start, setStart] = useState("");
     const [sleep_end, setEnd] = useState("");
-    const [sleep_quality, setQuality] = useState("");
-    const [drugs, setDrugs] = useState();
+    const [drugs, setDrugs] = useState("");
+    const [quality, setQuality] = useState("");
 
-    let sumbitData = {
-        "sleep_start": sleep_start,
-        "sleep_end": sleep_end,
-        "sleep_quality": sleep_quality,
-        "drugs": drugs
-    }
+
+
+
+    console.log(drugs)
 
     const submitAnswers = async () => {
         const response = await fetch(`http://localhost:${process.env.APIPORT}/test/${testId}/sleep`, {
             method: "PATCH",
-            body: JSON.stringify(sumbitData),
+            body: JSON.stringify(),
             headers: {
                 'Content-Type': 'application/json'
             }
         })
+        alert("test")
     }
 
     let msg = data.errors[0].msg
@@ -41,8 +41,8 @@ const Sleep = ({data}) => {
                     </div>
                     <div className="flex bg-gray-100 p-4 rounded-lg">
                         <input className="bg-gray-100 rounded-lg outline-none" type="datetime-local"
-                               min="2021-08-20T00:00" max="2021-11-30T00:00" value={sleep_start}
-                               onChange={(event => setStart(event.target.value))}/>
+                               min="2021-08-20T00:00" max="2021-11-30T00:00"
+                        onChange={(e) => setStart(e.target.value)}/>
                     </div>
                 </div>
 
@@ -52,8 +52,8 @@ const Sleep = ({data}) => {
                     </div>
                     <div className="flex justify-self-end bg-gray-100 p-4 space-x-4 rounded-lg">
                         <input className="bg-gray-100 rounded-lg outline-none" type="datetime-local"
-                               min="2021-08-20T00:00" max="2021-11-30T00:00" value={sleep_end}
-                               onChange={event => setEnd(event.target.value)}/>
+                               min="2021-08-20T00:00" max="2021-11-30T00:00"
+                               onChange={(e) => setEnd(e.target.value)}/>
                     </div>
                 </div>
 
@@ -64,12 +64,13 @@ const Sleep = ({data}) => {
 
                     <div className="flex-auto bg-gray-100 p-4 space-x-8 rounded-lg">
                         <label className="inline-flex space-x-2 items-center">
-                            <input type="radio" className="form-radio" name={drugs} value="1" onClick={setDrugs(1)}/>
+                            <input type="radio" className="form-radio" name="drugsbuttons" value="1"
+                                   onClick={(e) => setDrugs("1")}/>
                             <span>Ja</span>
                         </label>
                         <label className="inline-flex space-x-2 items-center">
-                            <input type="radio" className="form-radio" name={drugs} value="0" defaultChecked
-                                   onClick={setDrugs(0)}/>
+                            <input type="radio" className="form-radio" name="drugsbuttons" value="0"
+                                   onClick={(e) => setDrugs("0")}/>
                             <span>Nein</span>
                         </label>
                     </div>
@@ -78,12 +79,11 @@ const Sleep = ({data}) => {
 
                 <div className="flex items-center p-6 space-x-6 bg-white rounded-xl shadow-lg">
                     <div className="py-3 px-4 rounded-lg text-gray-500 font-semibold">
-                        <span>wie gut hast du geschlafen?<br/>(1 = sehr schlecht, 10 = sehr gut) </span>
+                        <span>Wie gut hast du geschlafen?<br/>(1 = sehr schlecht, 10 = sehr gut) </span>
                     </div>
                     <div className="flex justify-end bg-gray-100 p-4 space-x-8 rounded-lg">
                         <label className="inline-flex space-x-2 items-center rounded-lg">
-                            <select className="rounded-lg" name={sleep_quality}
-                                    onChange={event => setQuality(event.target.value)}>
+                            <select className="rounded-lg" onChange={(e) => setQuality(e.target.value)}>
                                 <option>1</option>
                                 <option>2</option>
                                 <option>3</option>
@@ -97,7 +97,11 @@ const Sleep = ({data}) => {
                             </select>
                         </label>
                     </div>
-
+                </div>
+                <div className="flex justify-center items-center py-2 px-4">
+                    <button type= "submit" className="bg-blue-700 hover:bg-blue-800 px-5 py-3 rounded-lg text-white text-xl duration-500" onSubmit={submitAnswers()}>
+                        absenden
+                    </button>
                 </div>
             </div>
         </div>
