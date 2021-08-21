0b0dfbf8cac9ff97495e89fe656b8a4c7b0b4d77
Knirpsii <michel.j.mosberger@gmail.com>
2021-08-21 11:56:43 +0200

    -

--- pages/test/[testId]/index.js
+++ pages/test/[testId]/index.js
@@ -1,190 +1,74 @@
 import {useRouter} from "next/router";
+import symbols from "../../../public/symbols.json"
+import {useEffect, useState} from "react";
+import React from "react"
 
 const Test = ({data}) => {
 
-    let legende = data.legende;
+    const handleSubmit = async (event) => {
+        event.preventDefault()
+
+
+        for (const element of event.target.elements) {
+            console.log(element.value);
+        }
+
+    }
 
     return (
-        <div className="bg-gray-100 min-w-screen min-h-screen">
-            <div className="flex items-center justify-center">
-                <div className="mt-5">
-                    <table className="table-fixed border-collapse border border-black">
-                        <thead>
-                        <tr>
-                            <th className="w-1/2">Title</th>
-                            <th className="w-1/4">Author</th>
-                            <th className="w-1/4">Views</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        <tr>
-                            <td className="border border-black">Intro to CSS</td>
-                            <td className="border border-black">Adam</td>
-                            <td className="border border-black">858</td>
-                        </tr>
-                        <tr className="bg-blue-200">
-                            <td className="border border-black">A Long and Winding Tour of the History of UI Frameworks
-                                and Tools and the Impact on
-                                Design
-                            </td>
-                            <td className="border border-black">Adam</td>
-                            <td className="border border-black">112</td>
-                        </tr>
-                        </tbody>
-                    </table>
-                </div>
+        <>
+
+            <div className="w-full h-full fixed z-10 top-0 left-0 bg-blue-200 lg:hidden">
+                <p className="flex justify-center align-center">Screen to small</p>
             </div>
 
-            <div className="m-5 flex justify-center">
-                <div className="flex mt-10 flex-wrap max-w-7xl">
-                    <div className="w-1/12">
-                        <div>
-                            <p>Icon</p>
-                            <input placeholder="test"/>
-                        </div>
-                    </div>
-                    <div className="w-1/12">
-                        <div>
-                            <p>Icon</p>
-                            <input placeholder="test"/>
-                        </div>
-                    </div>
-                    <div className="w-1/12">
-                        <div>
-                            <p>Icon</p>
-                            <input placeholder="test"/>
-                        </div>
-                    </div>
-                    <div className="w-1/12">
-                        <div>
-                            <p>Icon</p>
-                            <input placeholder="test"/>
-                        </div>
-                    </div>
-                    <div className="w-1/12">
-                        <div>
-                            <p>Icon</p>
-                            <input placeholder="test"/>
-                        </div>
-                    </div>
-                    <div className="w-1/12">
-                        <div>
-                            <p>Icon</p>
-                            <input placeholder="test"/>
-                        </div>
-                    </div>
-                    <div className="w-1/12">
-                        <div>
-                            <p>Icon</p>
-                            <input placeholder="test"/>
-                        </div>
-                    </div>
-                    <div className="w-1/12">
-                        <div>
-                            <p>Icon</p>
-                            <input placeholder="test"/>
-                        </div>
-                    </div>
-                    <div className="w-1/12">
-                        <div>
-                            <p>Icon</p>
-                            <input placeholder="test"/>
-                        </div>
-                    </div>
-                    <div className="w-1/12">
-                        <div>
-                            <p>Icon</p>
-                            <input placeholder="test"/>
-                        </div>
-                    </div>
-                    <div className="w-1/12">
-                        <div>
-                            <p>Icon</p>
-                            <input placeholder="test"/>
-                        </div>
-                    </div>
-                    <div className="w-1/12">
-                        <div>
-                            <p>Icon</p>
-                            <input placeholder="test"/>
-                        </div>
-                    </div>
-                    <div className="w-1/12">
-                        <div>
-                            <p>Icon</p>
-                            <input placeholder="test"/>
-                        </div>
-                    </div>
-                    <div className="w-1/12">
-                        <div>
-                            <p>Icon</p>
-                            <input placeholder="test"/>
-                        </div>
-                    </div>
-                    <div className="w-1/12">
-                        <div>
-                            <p>Icon</p>
-                            <input placeholder="test"/>
-                        </div>
-                    </div>
-                    <div className="w-1/12">
-                        <div>
-                            <p>Icon</p>
-                            <input placeholder="test"/>
-                        </div>
-                    </div>
-                    <div className="w-1/12">
-                        <div>
-                            <p>Icon</p>
-                            <input placeholder="test"/>
-                        </div>
-                    </div>
-                    <div className="w-1/12">
-                        <div>
-                            <p>Icon</p>
-                            <input placeholder="test"/>
-                        </div>
-                    </div>
-                    <div className="w-1/12">
-                        <div>
-                            <p>Icon</p>
-                            <input placeholder="test"/>
-                        </div>
-                    </div>
-                    <div className="w-1/12">
-                        <div>
-                            <p>Icon</p>
-                            <input placeholder="test"/>
-                        </div>
-                    </div>
-                    <div className="w-1/12">
-                        <div>
-                            <p>Icon</p>
-                            <input placeholder="test"/>
-                        </div>
-                    </div>
-                    <div className="w-1/12">
-                        <div>
-                            <p>Icon</p>
-                            <input placeholder="test"/>
-                        </div>
-                    </div>
-                    <div className="w-1/12">
-                        <div>
-                            <p>Icon</p>
-                            <input placeholder="test"/>
-                        </div>
-                    </div>
-                    <div className="w-1/12">
-                        <div>
-                            <p>Icon</p>
-                            <input placeholder="test"/>
-                        </div>
-                    </div>
-                </div>
+            <form onSubmit={(e) => {handleSubmit(e)}}>
+                <div className="bg-gray-100 min-w-screen min-h-screen">
+                    <div className="flex p-10 w-full">
+                        <div className="flex flex-wrap w-full items-center justify-center">
+                            {
+                                data.legende.map(number => (
 
-            </div>
-        </div>
+                                    <div className="border border-black justify-center w-12" key={number.icon_no}>
+                                        <p className="bg-gray-200 text-center text-2xl border border-rounded-none">{symbols.find(x => x.id === number.icon_id).symbol}</p>
+                                        <p className="text-center text-2xl border border-rounded-none">{number.icon_no}</p>
+                                    </div>
+
+                                ))
+                            }
+                        </div>
+                    </div>
+
+                    <div className="mt-10 mx-8">
+                        <div className="mt-10 flex justify-center">
+                            <div className="flex flex-wrap max-w-7xl ">
+                                {
+                                    data.answers.map(answer => (
+
+                                        <div className="w-1/25 border border-black justify-center mb-12"
+                                             key={answer.answer_no}>
+                                            <p className="bg-gray-200 text-center text-2xl border border-rounded-none">{symbols.find(x => x.id === answer.icon_id).symbol}</p>
+                                            <input type="number" min="1" max="9" className="w-full text-center text-2xl"
+                                                   name={answer.answer_no}/>
+                                        </div>
+
+                                    ))
+                                }
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <div className="flex items-center justify-center">
+                        <button type="submit"
+                                className="bg-transparent text-xl hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+                            senden
+                        </button>
+                    </div>
+
+                </div>
+            </form>
+        </>
     )
 }
 
@@ -199,7 +83,7 @@ Test.getInitialProps = async ({req, res, query}) => {
         await response.json().then(async resData => {
             data = resData
 
-            /*if (response.status !== 200) {
+            if (response.status !== 200) {
                 if (response.status === 401) {
                     res.writeHead(301, {
                         location: `/test/${query.testId}/sleep`
@@ -216,7 +100,7 @@ Test.getInitialProps = async ({req, res, query}) => {
                 return {
                     props: data,
                 }
-            }*/
+            }
         })
     })
 
